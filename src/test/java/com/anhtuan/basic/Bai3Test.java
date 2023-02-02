f26c742644bc347d5561187c41d807be27f3e590
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2023-02-02 21:31:50 +0700

    Bai 3

---
+++ src/test/java/com/anhtuan/basic/Bai3Test.java
@@ -0,0 +1,30 @@
+package com.anhtuan.basic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Bai3Test {
+
+    @Test
+    void testSumDigits() {
+        final var bai3 = new Bai3();
+
+        assertEquals(5 , bai3.sumDigits(32));
+        assertEquals(5 , bai3.sumDigits(23));
+        assertEquals(8 , bai3.sumDigits(44));
+        assertEquals(5 , bai3.sumDigits(5));
+    }
+
+    @Test
+    void testSortArray() {
+        final var bai3 = new Bai3();
+
+        int []input = new int[]{22, 123, 45, 67, 89, 21, 56, 23};
+        int []result = new int[]{21, 22, 23, 123, 45, 56, 67, 89};
+
+        assertEquals(result , bai3.handleSortArray(input));
+    }
+}
