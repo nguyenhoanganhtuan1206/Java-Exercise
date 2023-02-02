b888d3e5c6608a02229e3ee02f7dcbda47e68033
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2023-02-02 16:29:23 +0700

    fix

--- src/main/java/com/anhtuan/exercise/basic/Bai2.java
+++ src/main/java/com/anhtuan/exercise/basic/Bai2.java
@@ -1,11 +1,11 @@
 package com.anhtuan.exercise.basic;
 
 public class Bai2 {
-    public double sumDigits(final int number) {
-        final double sum = 0;
+    public int sumDigits(final int number) {
+        final int sum = 0;
 
         int tempNumber = number;
-        double result = sum;
+        int result = sum;
 
         while (tempNumber > 0) {
             result += tempNumber % 10;
