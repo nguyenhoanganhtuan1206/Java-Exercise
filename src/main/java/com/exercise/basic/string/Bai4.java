c7b01379781da287e0fe88a78e85456ff7d650f4
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2023-02-03 15:21:35 +0700

    Bai 4

--- src/main/java/com/exercise/basic/string/Bai4.java
+++ src/main/java/com/exercise/basic/string/Bai4.java
@@ -6,7 +6,7 @@ import java.util.Set;
 public class Bai4 {
 
     public boolean checkDuplicate(final String str) {
-        Set<Character> set = new HashSet<>();
+        final Set<Character> set = new HashSet<>();
 
         for (int i = 0; i < str.length(); i++) {
             set.add(str.charAt(i));
