c9442c268fe42f1914d2c04407f1927afe0f1d69
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2023-02-03 10:08:50 +0700

    fix bai 5

--- src/main/java/com/exercise/basic/Bai5.java
+++ src/main/java/com/exercise/basic/Bai5.java
@@ -1,8 +1,12 @@
-package com.anhtuan.basic;
+package com.exercise.basic;
 
 public class Bai5 {
 
     public int subtractNumber(final int[] arr) {
+        if(arr.length == 1) {
+            return arr[arr.length - 1];
+        }
+
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
 
