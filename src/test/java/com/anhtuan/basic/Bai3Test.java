bf6a28db2c409dfbd19fbf77c67df08e28beff47
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2023-02-03 07:32:07 +0700

    fix format

--- src/test/java/com/anhtuan/basic/Bai3Test.java
+++ src/test/java/com/anhtuan/basic/Bai3Test.java
@@ -10,19 +10,19 @@ class Bai3Test {
     void testSumDigits() {
         final var bai3 = new Bai3();
 
-        assertEquals(5 , bai3.sumDigits(32));
-        assertEquals(5 , bai3.sumDigits(23));
-        assertEquals(8 , bai3.sumDigits(44));
-        assertEquals(5 , bai3.sumDigits(5));
+        assertEquals(5, bai3.sumDigits(32));
+        assertEquals(5, bai3.sumDigits(23));
+        assertEquals(8, bai3.sumDigits(44));
+        assertEquals(5, bai3.sumDigits(5));
     }
 
     @Test
     void testSortArray() {
         final var bai3 = new Bai3();
 
-        int []input = new int[]{22, 123, 45, 67, 89, 21, 56, 23};
-        int []result = new int[]{21, 22, 23, 123, 45, 56, 67, 89};
+        final int[] input = new int[]{22, 123, 45, 67, 89, 21, 56, 23};
+        int[] result = new int[]{21, 22, 23, 123, 45, 56, 67, 89};
 
-        assertArrayEquals(result , bai3.handleSortArray(input));
+        assertArrayEquals(result, bai3.handleSortArray(input));
     }
 }
