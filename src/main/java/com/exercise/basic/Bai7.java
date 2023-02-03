5dc40e95dc498ed084feed8aa02bcfb3b17be73b
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2023-02-03 15:42:21 +0700

    Bai 7

---
+++ src/main/java/com/exercise/basic/Bai7.java
@@ -0,0 +1,31 @@
+package com.exercise.basic;
+
+public class Bai7 {
+
+    public boolean findSubstring(final String str1, final String str2) {
+        if (str2.trim().length() == 0 || str1.trim().length() == 0) {
+            return false;
+        }
+
+        final String strLarge = str1.length() > str2.length() ? str1 : str2;
+        final String strSmaller = str1.length() < str2.length() ? str1 : str2;
+        final int largeLength = str1.length() > str2.length() ? str1.length() : str2.length();
+        final int smallerLength = str1.length() < str2.length() ? str1.length() : str2.length();
+
+        for (int i = 0; i <= largeLength - smallerLength; i++) {
+            int j;
+            // j used to check length with str2Len
+            for (j = 0; j < smallerLength; j++) {
+                if (strLarge.charAt(i + j) != strSmaller.charAt(j)) {
+                    break;
+                }
+            }
+
+            if (j == smallerLength) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ src/test/java/com/exercise/basic/Bai7Test.java
@@ -0,0 +1,19 @@
+package com.exercise.basic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Bai7Test {
+
+    @Test
+    void findSubstring() {
+        final var bai7 = new Bai7();
+
+        assertEquals(true, bai7.findSubstring("anh tuan", "tu"));
+        assertEquals(true, bai7.findSubstring("anh tuan", "anh"));
+        assertEquals(false, bai7.findSubstring("anh tuan", "asdasd"));
+        assertEquals(false, bai7.findSubstring("anh tuan", " "));
+        assertEquals(false, bai7.findSubstring("anh tuan", ""));
+    }
+}
