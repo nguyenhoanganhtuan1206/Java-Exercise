34937ce19f16a23bdf699b2b3f70a853cb55895b
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2023-02-03 07:47:43 +0700

    Fix JUnit test with some case

--- src/main/java/com/exercise/basic/Bai1.java
+++ src/main/java/com/exercise/basic/Bai1.java
@@ -3,7 +3,7 @@ package com.exercise.basic;
 public class Bai1 {
 
     public int findMaxNumber(final int []arr) {
-        int max = 0;
+        int max = Integer.MIN_VALUE;
 
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] > max) {
--- src/test/java/com/exercise/basic/Bai1Test.java
+++ src/test/java/com/exercise/basic/Bai1Test.java
@@ -9,8 +9,8 @@ class Bai1Test {
     @Test
     void testFindMaxNumber() {
         final var bai1 = new Bai1();
-        int arr[] = new int[]{1, 2, 4, 2, 5, 22, 2, 7, 8, 0};
 
-        assertEquals(22, bai1.findMaxNumber(arr));
+        assertEquals(22, bai1.findMaxNumber(new int[]{1, 2, 4, 2, 5, 22, 2, 7, 8, 0}));
+        assertEquals(-1, bai1.findMaxNumber(new int[]{-1, -2, -3, -4, -5}));
     }
 }
