c3fd35fa1b9724f1ebb8040fe84c2b75935017af
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2023-02-05 15:39:23 +0700

    Fix bai 4

--- src/main/java/com/exercise/basic/SoHoc/Bai4.java
+++ src/main/java/com/exercise/basic/SoHoc/Bai4.java
@@ -2,20 +2,20 @@ package com.exercise.basic.SoHoc;
 
 public class Bai4 {
 
-    public int fibonacci(final int number) {
-        if (number <= 1) {
-            return number;
-        }
-
-        return fibonacci(number - 1) + fibonacci(number - 2);
-    }
-
     public int sumFibonacci(final int number) {
+        // 4 -> 1 + 1 + 2 + 3 -> 7
+        int f1 = 1;
+        int f2 = 1;
+        int f3;
         int sum = 0;
 
-        for (int i = 0; i < number && fibonacci(i) < number; i++) {
-            sum += fibonacci(i);
+        for (int i = 1; f1 <= number; i++) {
+            sum += f1;
+            f3 = f1 + f2;
+            f1 = f2;
+            f2 = f3;
         }
+
         return sum;
     }
 }
