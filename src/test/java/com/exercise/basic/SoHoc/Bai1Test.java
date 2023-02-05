9192b7df971626068270cae8877fd7b4d81ae54f
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2023-02-05 16:12:26 +0700

    Fix test

--- src/test/java/com/exercise/basic/SoHoc/Bai1Test.java
+++ src/test/java/com/exercise/basic/SoHoc/Bai1Test.java
@@ -10,6 +10,8 @@ class Bai1Test {
     void testXuLyUocChungLonNhat() {
         final var bai1 = new Bai1();
 
+        assertEquals(10, bai1.xuLyUocChungLonNhat(10, 0));
+        assertEquals(10, bai1.xuLyUocChungLonNhat(0, 10));
         assertEquals(5, bai1.xuLyUocChungLonNhat(15, 20));
         assertEquals(1, bai1.xuLyUocChungLonNhat(15, 13));
     }
@@ -18,6 +20,8 @@ class Bai1Test {
     void testXuLyBoiChungNhoNhat() {
         final var bai1 = new Bai1();
 
+        assertEquals(0, bai1.xuLyBoiChungNhoNhat(20, 0));
+        assertEquals(0, bai1.xuLyBoiChungNhoNhat(0, 20));
         assertEquals(20, bai1.xuLyBoiChungNhoNhat(20, 5));
         assertEquals(60, bai1.xuLyBoiChungNhoNhat(15, 20));
         assertEquals(36, bai1.xuLyBoiChungNhoNhat(12, 18));
