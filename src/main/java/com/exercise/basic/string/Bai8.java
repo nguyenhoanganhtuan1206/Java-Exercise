ed3cd9c1838069d6b91f20fa753197ec834faaf4
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2023-02-06 06:27:11 +0700

    Fix bai 8

--- src/main/java/com/exercise/basic/string/Bai8.java
+++ src/main/java/com/exercise/basic/string/Bai8.java
@@ -3,21 +3,21 @@ package com.exercise.basic.string;
 public class Bai8 {
 
     public boolean handleCheckStr(final String str) {
-        int countOpenBracket = 0;
-        int countCloseBracket = 0;
+        // COV down
+        int countLen = 0;
 
         for (int i = 0; i < str.length(); i++) {
             if (str.charAt(i) == '(') {
-                countOpenBracket++;
+                countLen++;
             } else if (str.charAt(i) == ')') {
-                countCloseBracket++;
+                countLen--;
             } else {
                 // Check other characters
                 return false;
             }
         }
 
-        if (countCloseBracket == countOpenBracket) {
+        if (countLen == 0) {
             return true;
         }
 
--- src/test/java/com/exercise/basic/string/Bai8Test.java
+++ src/test/java/com/exercise/basic/string/Bai8Test.java
@@ -1,6 +1,5 @@
 package com.exercise.basic.string;
 
-import com.exercise.basic.string.Bai8;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
