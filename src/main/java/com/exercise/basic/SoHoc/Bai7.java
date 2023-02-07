2a66972bcfb83798eebb4a96ee735f93e10fa392
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2023-02-07 16:03:52 +0700

    Bai 7

---
+++ src/main/java/com/exercise/basic/SoHoc/Bai7.java
@@ -0,0 +1,21 @@
+package com.exercise.basic.SoHoc;
+
+public class Bai7 {
+
+    public boolean isCheck(int n) {
+        final String numberStr = String.valueOf(n);
+
+        for (int j = 2; j <= 5; j++) {
+            int sum = 0;
+            for (int i = 0; i < numberStr.length(); i++) {
+                int numberTemp = Integer.parseInt(String.valueOf(numberStr.charAt(i)));
+                sum += Math.pow(numberTemp, j);
+            }
+            if (sum == n) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ src/test/java/com/exercise/basic/SoHoc/Bai7Test.java
@@ -0,0 +1,18 @@
+package com.exercise.basic.SoHoc;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Bai7Test {
+
+    @Test
+    void isCheck() {
+        final var bai7 = new Bai7();
+
+        assertTrue(bai7.isCheck(153));
+
+        assertFalse(bai7.isCheck(23));
+        assertFalse(bai7.isCheck(154));
+    }
+}
