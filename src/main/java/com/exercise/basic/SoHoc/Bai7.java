072fc66e2069026652eb269a7433ceeb7954ec90
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2023-02-07 21:50:54 +0700

    Fix bai 7

--- src/main/java/com/exercise/basic/SoHoc/Bai7.java
+++ src/main/java/com/exercise/basic/SoHoc/Bai7.java
@@ -2,16 +2,21 @@ package com.exercise.basic.SoHoc;
 
 public class Bai7 {
 
-    public boolean checkSum(int n) {
+    public int getSum(final int n, final int pow) {
         final String numberStr = String.valueOf(n);
+        int sum = 0;
 
+        for (int i = 0; i < numberStr.length(); i++) {
+            int numberTemp = Integer.parseInt(numberStr.charAt(i) + "");
+            sum += Math.pow(numberTemp, pow);
+        }
+
+        return sum;
+    }
+
+    public boolean checkSum(final int n) {
         for (int j = 2; j <= 5; j++) {
-            int sum = 0;
-            for (int i = 0; i < numberStr.length(); i++) {
-                int numberTemp = Integer.parseInt(numberStr.charAt(i) + "");
-                sum += Math.pow(numberTemp, j);
-            }
-            if (sum == n) {
+            if (getSum(n, j) == n) {
                 return true;
             }
         }
