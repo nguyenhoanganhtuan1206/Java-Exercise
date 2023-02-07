48248886b78652aaeeea9518b5b87eda7b8aaf6e
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2023-02-07 07:49:38 +0700

    bai 2

--- src/test/java/com/exercise/basic/SoHoc/Bai2Test.java
+++ src/test/java/com/exercise/basic/SoHoc/Bai2Test.java
@@ -2,7 +2,7 @@ package com.exercise.basic.SoHoc;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Bai2Test {
 
@@ -10,9 +10,8 @@ class Bai2Test {
     void sumDigits() {
         final var bai2 = new Bai2();
 
-        assertEquals(10 , bai2.sumDigits(91));
-        assertEquals(10 , bai2.sumDigits(82));
-        assertEquals(-10 , bai2.sumDigits(-82));
-        assertEquals(1 , bai2.sumDigits(1));
+        assertEquals(10, bai2.sumDigits(91));
+        assertEquals(10, bai2.sumDigits(82));
+        assertEquals(1, bai2.sumDigits(1));
     }
 }
