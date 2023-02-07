8082255889387dbb0da94be36cf1e4bc8067ee80
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2023-02-07 10:20:32 +0700

    Bai 6

--- src/main/java/com/exercise/basic/string/Bai6.java
+++ src/main/java/com/exercise/basic/string/Bai6.java
@@ -1,7 +1,5 @@
 package com.exercise.basic.string;
 
-import java.security.InvalidAlgorithmParameterException;
-
 public class Bai6 {
 
     public boolean isBarcode(final String str) {
@@ -19,11 +17,7 @@ public class Bai6 {
         return (sumEven + 3 * sumOdd) % 10 == 0;
     }
 
-    public int generatorLastNumBarcode(final String str) throws Exception {
-        if (str.length() < 12) {
-            throw new InvalidAlgorithmParameterException("Barcode must be larger than 12 length");
-        }
-
+    public int generatorLastNumBarcode(final String str) {
         for (int i = 0; i < 10; i++) {
             if (isBarcode(str.concat(i + ""))) {
                 return i;
--- src/test/java/com/exercise/basic/string/Bai6Test.java
+++ src/test/java/com/exercise/basic/string/Bai6Test.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class Bai6Test {
 
     @Test
-    void generatorLastNumBarcode() throws Exception {
+    void generatorLastNumBarcode() {
         final var bai6 = new Bai6();
 
         assertEquals(4, bai6.generatorLastNumBarcode("978020137962"));
