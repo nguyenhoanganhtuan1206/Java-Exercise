ba734afa5e39053ca0f356c3f99c8e703afb6910
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2023-02-08 11:50:28 +0700

    fix test Bai 9

--- src/main/java/com/exercise/basic/SoHoc/Bai9.java
+++ src/main/java/com/exercise/basic/SoHoc/Bai9.java
@@ -2,9 +2,9 @@ package com.exercise.basic.SoHoc;
 
 public class Bai9 {
 
-    public double calSqrt(final double number) {
+    public double calSqrt(final double number) throws Exception {
         if (number < 0) {
-            return Double.NaN;
+            throw new Exception("Number must be larger than 0");
         }
 
         double start = 0;
