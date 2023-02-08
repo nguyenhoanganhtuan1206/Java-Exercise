d711b4801929d9b560ff3a7c8ba4d430e78aad38
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2023-02-08 11:25:02 +0700

    Fix bai 7

--- src/main/java/com/exercise/basic/SoHoc/Bai7.java
+++ src/main/java/com/exercise/basic/SoHoc/Bai7.java
@@ -2,19 +2,23 @@ package com.exercise.basic.SoHoc;
 
 public class Bai7 {
 
+    public int calculatePow(final int digit, final int pow) {
+        int poweredDigit = 1;
+        for (int i = 0; i < pow; i++) {
+            poweredDigit *= digit;
+        }
+
+        return poweredDigit;
+    }
+
     public int getSum(final int number, final int pow) {
         int numberTemp = number;
         int sum = 0;
 
         while (numberTemp != 0) {
             int digit = numberTemp % 10;
-            int poweredDigit = 1;
 
-            // pow = 5 -> i->5
-            for (int i = 0; i < pow; i++) {
-                poweredDigit *= digit;
-            }
-            sum += poweredDigit;
+            sum += calculatePow(digit, pow);
             numberTemp /= 10;
         }
 
