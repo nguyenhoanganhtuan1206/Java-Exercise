add77558c366731d8b870936291a21466187f704
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2023-02-08 09:41:56 +0700

    Fix bai 6

--- src/main/java/com/exercise/basic/string/Bai6.java
+++ src/main/java/com/exercise/basic/string/Bai6.java
@@ -20,7 +20,7 @@ public class Bai6 {
     public int generatorLastNumBarcode(final String str) {
         for (int i = '0'; i <= '9'; i++) {
             if (isBarcode(str + i)) {
-                return Integer.parseInt(i + "");
+                return i - '0';
             }
         }
 
--- src/test/java/com/exercise/basic/string/Bai6Test.java
+++ src/test/java/com/exercise/basic/string/Bai6Test.java
@@ -10,8 +10,8 @@ class Bai6Test {
     void generatorLastNumBarcode() {
         final var bai6 = new Bai6();
 
-        assertEquals(4, bai6.generatorLastNumBarcode("978020137962"));
-        assertEquals(4, bai6.generatorLastNumBarcode("893850597419"));
-        assertEquals(8, bai6.generatorLastNumBarcode("893850597411"));
+        assertEquals(7, bai6.generatorLastNumBarcode("978020137962"));
+        assertEquals(7, bai6.generatorLastNumBarcode("893850597419"));
+        assertEquals(5, bai6.generatorLastNumBarcode("893850597411"));
     }
 }
