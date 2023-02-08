14ec42646a0bd03cc7256068f1bbb49723d8440a
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2023-02-08 07:49:01 +0700

    Fix test bai 3

--- src/test/java/com/exercise/basic/string/Bai3Test.java
+++ src/test/java/com/exercise/basic/string/Bai3Test.java
@@ -2,7 +2,7 @@ package com.exercise.basic.string;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Bai3Test {
 
@@ -10,8 +10,12 @@ class Bai3Test {
     void sumDigits() {
         final var bai3 = new Bai3();
 
-        assertEquals(380 , bai3.sumDigits("abc 123 def 33 mn 3.221"));
-        assertEquals(156 , bai3.sumDigits("abc 123 def 33"));
-        assertEquals(0 , bai3.sumDigits("abcdef"));
+        assertEquals(380, bai3.sumDigits("abc 123 def 33 mn 3.221"));
+        assertEquals(156, bai3.sumDigits("abc 123 def 33"));
+        assertEquals(0, bai3.sumDigits("abcdef"));
+
+        assertEquals(123, bai3.sumDigits("123"));
+        assertEquals(123, bai3.sumDigits("123nnnnnnnnnnnnn"));
+        assertEquals(213123213, bai3.sumDigits("n213123213n"));
     }
 }
