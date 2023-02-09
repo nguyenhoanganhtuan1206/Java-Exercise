8abb3d10279b6714fbdf0109da936dccd24465a0
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2023-02-09 09:42:58 +0700

    Fix bai 6

--- src/main/java/com/exercise/basic/string/Bai6.java
+++ src/main/java/com/exercise/basic/string/Bai6.java
@@ -2,30 +2,17 @@ package com.exercise.basic.string;
 
 public class Bai6 {
 
-    public boolean isBarcode(final String str) {
-        int sumEven = 0;
-        int sumOdd = 0;
+    public int generatorLastBarcode(final String str) {
+        int sum = 0;
 
         for (int i = 0; i < str.length(); i++) {
             if (i % 2 == 0) {
-                sumEven += str.charAt(i);
+                sum += 3 * (str.charAt(i) - '0');
             } else {
-                sumOdd += str.charAt(i);
+                sum += str.charAt(i) - '0';
             }
         }
 
-        return (sumEven + 3 * sumOdd) % 10 == 0;
-    }
-
-    public int generatorLastNumBarcode(final String str) {
-        int lastNumber = 0;
-
-        for (int i = '0'; i <= '9'; i++) {
-            if (isBarcode(str + i)) {
-                lastNumber = i - '0';
-            }
-        }
-
-        return lastNumber;
+        return 10 - (sum % 10);
     }
 }
