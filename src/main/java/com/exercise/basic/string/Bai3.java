7ced09cbdf22265d1ec726ff974a0ecab3128158
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2023-02-10 07:22:56 +0700

    Fix bai 3

--- src/main/java/com/exercise/basic/string/Bai3.java
+++ src/main/java/com/exercise/basic/string/Bai3.java
@@ -3,26 +3,26 @@ package com.exercise.basic.string;
 public class Bai3 {
 
     public int sumDigits(final String str) {
-        final StringBuilder strResult = new StringBuilder();
-
+        int number = 0;
         int sum = 0;
 
         for (int i = 0; i < str.length(); i++) {
             char c = str.charAt(i);
 
             if (Character.isDigit(c)) {
-                strResult.append(c);
+                // nhân 10 lên hàng chục or trăm
+                number = number * 10 + (c - '0');
             } else {
-                // Check ở có kí tự và khoảng trắng thì cộng
-                if (strResult.length() > 0) {
-                    sum += Integer.parseInt(strResult.toString());
+                // Check if there are characters and spaces, then add the sum to the previous sum
+                if (number > 0) {
+                    sum += number;
+                    number = 0;
                 }
-                strResult.setLength(0);
             }
         }
 
-        if (strResult.length() > 0) {
-            sum += Integer.parseInt(strResult.toString());
+        if (number > 0) {
+            sum += number;
         }
 
         return sum;
