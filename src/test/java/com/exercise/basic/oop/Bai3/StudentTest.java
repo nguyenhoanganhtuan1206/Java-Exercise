ad959bad796ee8aaa4e5dfc36a75958188dba7f7
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2023-02-14 17:34:06 +0700

    Fix

--- src/test/java/com/exercise/basic/oop/Bai3/StudentTest.java
+++ src/test/java/com/exercise/basic/oop/Bai3/StudentTest.java
@@ -10,7 +10,7 @@ public class StudentTest {
 
     @Test
     void testStudent() {
-        final var student1 = Student.builder()
+        final var student = Student.builder()
                 .firstName("Nguyen Hoang")
                 .lastName("Anh Tuan")
                 .address("Da Nang")
@@ -18,16 +18,17 @@ public class StudentTest {
                 .email("nguyenhanhtuan@gmail.com")
                 .gender(Gender.MALE)
                 .className("ABC")
-                .phone("090xxxxx");
+                .phone("090xxxxx")
+                .build();
 
 
-        assertEquals("Nguyen Hoang", student1.build().getFirstName());
-        assertEquals("Anh Tuan", student1.build().getLastName());
-        assertEquals("ABC", student1.build().getClassName());
-        assertEquals("nguyenhanhtuan@gmail.com", student1.build().getEmail());
-        assertEquals(LocalDate.of(2001, 06, 12), student1.build().getBirthday());
-        assertEquals("Da Nang", student1.build().getAddress());
-        assertEquals("090xxxxx", student1.build().getPhone());
-        assertEquals(Gender.MALE, student1.build().getGender());
+        assertEquals("Nguyen Hoang", student.getFirstName());
+        assertEquals("Anh Tuan", student.getLastName());
+        assertEquals("ABC", student.getClassName());
+        assertEquals("nguyenhanhtuan@gmail.com", student.getEmail());
+        assertEquals(LocalDate.of(2001, 06, 12), student.getBirthday());
+        assertEquals("Da Nang", student.getAddress());
+        assertEquals("090xxxxx", student.getPhone());
+        assertEquals(Gender.MALE, student.getGender());
     }
 }
