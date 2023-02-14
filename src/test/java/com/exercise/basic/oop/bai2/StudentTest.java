b53e3b1f80c7da23225d51e805647c8307ea38e6
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2023-02-14 15:50:34 +0700

    Fix jUnit TEST

--- src/test/java/com/exercise/basic/oop/bai2/StudentTest.java
+++ src/test/java/com/exercise/basic/oop/bai2/StudentTest.java
@@ -8,18 +8,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StudentTest {
 
+    StudentBuilder studentBuilder = new StudentConcreteBuilder()
+            .setFirstName("Nguyen Hoang")
+            .setLastName("Anh Tuan")
+            .setAddress("Da Nang")
+            .setClassName("ABC")
+            .setEmail("nguyenhanhtuan@gmail.com")
+            .setBirthDay(LocalDate.of(2001, 06, 12))
+            .setPhone("090xxxxx")
+            .setGender(Gender.MALE);
+
     @Test
     void testStudent() {
-        StudentBuilder studentBuilder = new StudentConcreteBuilder()
-                .setFirstName("Nguyen Hoang")
-                .setLastName("Anh Tuan")
-                .setAddress("Da Nang")
-                .setClassName("ABC")
-                .setEmail("nguyenhanhtuan@gmail.com")
-                .setBirthDay(LocalDate.of(2001, 06, 12))
-                .setPhone("090xxxxx")
-                .setGender(Gender.MALE);
-
         assertEquals("Nguyen Hoang", studentBuilder.build().getFirstName());
         assertEquals("Anh Tuan", studentBuilder.build().getLastName());
         assertEquals("ABC", studentBuilder.build().getClassName());
@@ -28,5 +28,108 @@ public class StudentTest {
         assertEquals("Da Nang", studentBuilder.build().getAddress());
         assertEquals("090xxxxx", studentBuilder.build().getPhone());
         assertEquals(Gender.MALE, studentBuilder.build().getGender());
+    }
+
+    @Test
+    void testToString() {
+        assertEquals(studentBuilder.build().toString(), "Student{" +
+                "firstName='" + studentBuilder.build().getFirstName() + '\'' +
+                ", lastName='" + studentBuilder.build().getLastName() + '\'' +
+                ", birthDay=" + studentBuilder.build().getBirthDay() +
+                ", className='" + studentBuilder.build().getClassName() + '\'' +
+                ", address='" + studentBuilder.build().getAddress() + '\'' +
+                ", gender=" + studentBuilder.build().getGender() +
+                ", email='" + studentBuilder.build().getEmail() + '\'' +
+                ", phone='" + studentBuilder.build().getPhone() + '\'' +
+                '}');
+    }
+
+    @Test
+    void getFirstName() {
+        assertEquals("Nguyen Hoang", studentBuilder.build().getFirstName());
+    }
+
+    @Test
+    void setFirstName() {
+        studentBuilder.setFirstName("Tuan");
+        assertEquals("Tuan", studentBuilder.build().getFirstName());
+
+    }
+
+    @Test
+    void getLastName() {
+        assertEquals("Anh Tuan", studentBuilder.build().getLastName());
+    }
+
+    @Test
+    void setLastName() {
+        studentBuilder.setLastName("Tuan");
+        assertEquals("Tuan", studentBuilder.build().getLastName());
+    }
+
+    @Test
+    void getBirthDay() {
+        assertEquals(LocalDate.of(2001, 06, 12), studentBuilder.build().getBirthDay());
+    }
+
+    @Test
+    void setBirthDay() {
+        studentBuilder.setBirthDay(LocalDate.of(2001, 05, 12));
+        assertEquals(LocalDate.of(2001, 05, 12), studentBuilder.build().getBirthDay());
+    }
+
+    @Test
+    void getClassName() {
+        assertEquals("ABC", studentBuilder.build().getClassName());
+    }
+
+    @Test
+    void setClassName() {
+        studentBuilder.setClassName("ABCD");
+        assertEquals("ABCD", studentBuilder.build().getClassName());
+    }
+
+    @Test
+    void getAddress() {
+        assertEquals("Da Nang", studentBuilder.build().getAddress());
+    }
+
+    @Test
+    void setAddress() {
+        studentBuilder.setAddress("Da Nang");
+        assertEquals("Da Nang", studentBuilder.build().getAddress());
+    }
+
+    @Test
+    void getGender() {
+        assertEquals(Gender.MALE, studentBuilder.build().getGender());
+    }
+
+    @Test
+    void setGender() {
+        studentBuilder.setGender(Gender.FEMALE);
+        assertEquals(Gender.FEMALE, studentBuilder.build().getGender());
+    }
+
+    @Test
+    void getEmail() {
+        assertEquals("nguyenhanhtuan@gmail.com", studentBuilder.build().getEmail());
+    }
+
+    @Test
+    void setEmail() {
+        studentBuilder.setEmail("nguyenhanhtuan123@gmail.com");
+        assertEquals("nguyenhanhtuan123@gmail.com", studentBuilder.build().getEmail());
+    }
+
+    @Test
+    void getPhone() {
+        assertEquals("090xxxxx", studentBuilder.build().getPhone());
+    }
+
+    @Test
+    void setPhone() {
+        studentBuilder.setPhone("09033213");
+        assertEquals("09033213", studentBuilder.build().getPhone());
     }
 }
