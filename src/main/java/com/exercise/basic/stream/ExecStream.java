b4e250281f55aa919dd6d7d36bc719ca63904974
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2023-02-20 09:05:49 +0700

    Fix Stream

--- src/main/java/com/exercise/basic/stream/ExecStream.java
+++ src/main/java/com/exercise/basic/stream/ExecStream.java
@@ -6,25 +6,23 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 public class ExecStream {
 
     public List<String> findVietnameseAddressByCommuneName(String communeName) throws IOException {
-        List<Commune> communes = findCommuneByName(communeName);
-        List<District> districts = findDistrictByCommunes(communes);
-        List<Province> provinces = findProvinceByDistricts(districts);
+        final List<Commune> communes = findCommuneByName(communeName);
+        final List<District> districts = findDistrictByCommunes(communes);
+        final List<Province> provinces = findProvinceByDistricts(districts);
 
-        StringBuilder str = new StringBuilder();
-        List<String> result = new ArrayList<>();
+        final StringBuilder str = new StringBuilder();
+        final List<String> result = new ArrayList<>();
 
         for (Commune commune : communes) {
             str.append(commune.getName() + ", ");
 
-            District district = districts.stream()
+            final District district = districts.stream()
                     .filter(d -> d.getIdDistrict().equals(commune.getIdDistrict()))
                     .findFirst()
                     .orElse(null);
@@ -33,7 +31,7 @@ public class ExecStream {
                 str.append(district.getName() + ", ");
             }
 
-            Province province = provinces.stream()
+            final Province province = provinces.stream()
                     .filter(p -> p.getIdProvince().equals(district.getIdProvince()))
                     .findFirst()
                     .orElse(null);
@@ -56,20 +54,20 @@ public class ExecStream {
         return mapper.readValue(data, VietnameseAddress.class);
     }
 
-    public List<Commune> findCommuneByName(String communeName) throws IOException {
+    public List<Commune> findCommuneByName(final String communeName) throws IOException {
         final VietnameseAddress vietnameseAddress = parseVietnameseAddress();
 
-        List<Commune> commune = vietnameseAddress.getCommunes().stream()
+        final List<Commune> commune = vietnameseAddress.getCommunes().stream()
                 .filter(c -> c.getName().contains(communeName))
                 .collect(Collectors.toList());
 
         return commune;
     }
 
-    public List<District> findDistrictByCommunes(List<Commune> communes) throws IOException {
+    public List<District> findDistrictByCommunes(final List<Commune> communes) throws IOException {
         final VietnameseAddress vietnameseAddress = parseVietnameseAddress();
 
-        List<District> districts = communes.stream()
+        final List<District> districts = communes.stream()
                 .flatMap(c -> {
                     String districtId = c.getIdDistrict();
 
@@ -81,10 +79,10 @@ public class ExecStream {
         return districts;
     }
 
-    public List<Province> findProvinceByDistricts(List<District> districts) throws IOException {
+    public List<Province> findProvinceByDistricts(final List<District> districts) throws IOException {
         final VietnameseAddress vietnameseAddress = parseVietnameseAddress();
 
-        List<Province> provinces = districts.stream()
+        final List<Province> provinces = districts.stream()
                 .flatMap(d -> {
                     String provinceId = d.getIdProvince();
 
