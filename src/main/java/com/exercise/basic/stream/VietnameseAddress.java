a9609328be782bb4441b4b814b841db32153aa8d
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2023-02-21 09:05:46 +0700

    Fix bugs

--- src/main/java/com/exercise/basic/stream/VietnameseAddress.java
+++ src/main/java/com/exercise/basic/stream/VietnameseAddress.java
@@ -1,12 +1,10 @@
 package com.exercise.basic.stream;
 
 import lombok.Getter;
-import lombok.ToString;
 
 import java.util.List;
 
 @Getter
-@ToString
 public class VietnameseAddress {
 
     private List<Commune> communes;
